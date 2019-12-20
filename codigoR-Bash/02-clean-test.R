56665ad1cb3b59490250b292eb8d8d72e5ff1dda
Javier Goujon <42042760+valencig@users.noreply.github.com>
2019-12-19 20:46:06 -0600

    Delete 02-clean-test.R
    nada

--- codigoR-Bash/02-clean-test.R
+++
@@ -1,8 +0,0 @@
-walmart_test <- walmart_test %>% mutate_at(c(5),funs(walmart_clean_data_test))
-
-#walmart_data <- walmart_data %>% mutate_at(c(3,6),funs(walmart_clean_factor_data))
-
-walmart_test <- readr::type_convert(walmart_test)
-
-walmart_test  <- walmart_test %>% 
-  mutate_at(c(2,3,6),funs(as.factor))
